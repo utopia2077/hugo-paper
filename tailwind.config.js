c05fe8e504b1885ef3aed5fcde8ead21550838f8
utopia2077 <zai@meng.li>
2024-12-03 00:58:03 +0800

    修改字体为霞鹜文楷屏幕版，优化中文显示效果;修复markdown反引号显示问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,33 @@ module.exports = {
   content: ['./layouts/**/*.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'wenkai': ['LXGW WenKai', 'sans-serif'],
+        'wenkai-mono': ['LXGW WenKai Mono', 'monospace'],
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              /** FIXME 颜色也太丑了 */
+              backgroundColor: theme('colors.slate.100'),
+              borderRadius: theme('borderRadius.sm'),
+              paddingTop: theme('padding[1]'),
+              paddingRight: theme('padding[1.5]'),
+              paddingBottom: theme('padding[1]'),
+              paddingLeft: theme('padding[1.5]'),
+            },
+            'code::before': {
+              content: 'normal',
+            },
+            'code::after': {
+              content: 'normal',
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
